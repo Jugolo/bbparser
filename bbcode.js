54e86b48e685a5df229850b5b460783b8ed36dc7
Ronnie <rix17172@gmail.com>
2017-01-30 00:03:04 +0100

    Update bbcode.js

--- bbcode.js
+++ bbcode.js
@@ -1,8 +1,53 @@
 var Bbcode = (
+  function renderBlock(state){
+    var result = {};
+    result.code = getCode(state);
+    if(state.s.charAt(state.i) === "="){
+      result.attribute = "";
+      state.i++;
+      for(;state.i<state.s.length;state.i++){
+        if(state.s.charAt(state.i) == "]"){
+          break;
+        }
+        result.attribute += state.s.charAt(state.i);
+      }
+    }
+    if(state.s.charAt(state.i) !== "]"){
+      return null;
+    }
+    return result;
+  }
+  
+  function renderString(obj, state){
+    var buffer = "";
+    
+    for(;state.i<state.s.length;state.i++){
+      if(state.s.charAt(state.i) == "["){
+        var i=state.i;//save the pos
+        var data = renderBlock(state);
+        if(data !== null && type.bbcode[data.code] !== "undefined"){
+          
+        }
+        state.i = i;
+      }
+      
+      buffer += state.s.charAt(state.i);
+    }
+    
+    return buffer;
+  }
   
   function Bbcode(){
-  
+    this.bbcode = {};
   }
+  
+  Bbcode.prototype.render = function(str){
+    var state = {
+      i : 0,
+      s : str
+    };
+    return renderString(this, state);
+  };
   
   return Bbcode;
 )();
